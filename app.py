e4a919838f148f9d664552928b5b2cd53a45284f
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-11-09 11:12:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import paho.mqtt.client as paho
 import json
 from gtts import gTTS
 from googletrans import Translator
+import numpy as np
 
 def on_publish(client,userdata,result):             #create function for callback
     print("el dato ha sido publicado \n")
@@ -82,3 +83,22 @@ if result:
         os.mkdir("temp")
     except:
         pass
+        
+
+#Boton de sonido
+
+audio_file = open('Dance.mp3', 'rb')
+audio_bytes = audio_file.read()
+
+st.audio(audio_bytes, format='audio/ogg')
+
+
+#sample_rate = 44100  # 44100 samples per second
+#seconds = 2  # Note duration of 2 seconds
+#frequency_la = 440  # Our played note will be 440 Hz
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+#t = np.linspace(0, seconds, seconds * sample_rate, False)
+# Generate a 440 Hz sine wave
+#note_la = np.sin(frequency_la * t * 2 * np.pi)
+
+st.audio(note_la, sample_rate=sample_rate)
