d40bfd8ede6eb93f9705631b546c585f381822e9
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-11-15 20:41:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ client1.on_message = on_message
 
 
 
-st.title(":pink[Espejo mágico App]")
+st.title(":violet[Espejo mágico App]")
 st.subheader("Controla tu espejo por voz")
 
 image = Image.open('EspejoMagico.jpg')
